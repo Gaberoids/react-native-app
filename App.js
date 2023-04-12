92f40b789ecbce9a2d413b46c0db5dc4731585a9
Gaberoids <figueiropas@hotmail.com>
2023-04-12 16:29:21 -0600

    Populating list of goals beggining.

--- App.js
+++ App.js
@@ -4,7 +4,10 @@ import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 // this is the root component all components must be inside it. Like index.html
 export default function App() {
+  // enteredGoalText is the Key, setEnteredGoalText is the value of the key.
   const [EnteredGoalText, setEnteredGoalText] = useState('');
+  // constant is an empty array to store the list of goals.
+  const[courseGoals, SetCourseGoals] = useState([])
 
   //input field goal handler
   function goalInputHandler(enteredText) {
@@ -14,9 +17,12 @@ export default function App() {
 
   // add goal to list
   function addGoalHandler(){
-    console.log(EnteredGoalText);
+    console.log("clicked button value = " + EnteredGoalText);
+    // optional, use modern react (...) => SetCourseGoals([...courseGoals, EnteredGoalText]);
+    SetCourseGoals(currentCourseGoals => [...currentCourseGoals, EnteredGoalText]);
+    //currentCourseGoals is automatically provided by react (useState I think)
   };
-  
+
   return (
     <View style={styles.appContainer}>
       <View style={styles.inputContainer}>
@@ -29,6 +35,7 @@ export default function App() {
       </View>
       <View style={styles.goalsContainer}>
           <Text> List of goals...</Text>
+          {courseGoals.map((goal) => <Text key={'key' + goal}>Goal x = {goal}</Text>)}
       </View>
     </View>
   );
