b8a308693ae7803fcacc4d1f62d9e03028c7f058
Gaberoids <figueiropas@hotmail.com>
2023-04-11 23:19:14 -0600

    Added text, button and styled these new elements inline and via style objects.

--- App.js
+++ App.js
@@ -1,15 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
+// this is the root component all components must be inside it. Like index.html
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hola Mundo!!!</Text>
-      <StatusBar style="auto" />
+      <Text style={{ margin:16, borderWidth: 2, borderColor: 'red', padding:16 }}> Another text</Text>
+      <Text 
+        style={styles.dummyText}>
+        Hola Mundo!!!
+      </Text>
+      <Button title='Tap me' />
     </View>
   );
 }
 
+// this is the style of app above since react native does not use CSS. Style objects, not inline style
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -17,4 +22,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  dummyText: {
+    margin:16,
+    borderWidth: 2,
+    borderColor: 'blue',
+    padding:16,
+  }
 });
