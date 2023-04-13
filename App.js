ddce24ba5de4737bf2500095048e748844f7246c
Gaberoids <figueiropas@hotmail.com>
2023-04-12 19:07:54 -0600

    List rendered with ScrollView and FlatList

--- App.js
+++ App.js
@@ -1,6 +1,14 @@
 // useState save the value in a function and allow to be used on other function
 import { useState } from 'react';
-import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TextInput,
+  ScrollView,
+  FlatList
+  } from 'react-native';
 
 // this is the root component all components must be inside it. Like index.html
 export default function App() {
@@ -34,14 +42,35 @@ export default function App() {
           <Button title='Add Goal' onPress={addGoalHandler}/>
       </View>
       <View style={styles.goalsContainer}>
-          <Text> List of goals...</Text>
-          {courseGoals.map((goal) => <Text key={'key' + goal}>Goal x = {goal}</Text>)}
+        <Text> FlatList = loads list items as you scrolldown...</Text>
+        <FlatList data={courseGoals}
+          /* renderItem and itemData are from flatList. itemData is each Item from the array/list and some metadata */
+          renderItem={(itemData) => {
+            // itemData.index is built in from FlatList too
+            itemData.index
+            return (
+              <View style={styles.goalItem} >
+                <Text style={styles.goalText} >Goal x = {itemData.item}</Text>
+              </View>
+            );
+          }}
+          
+          alwaysBounceVertical={false} />
+
+        <Text> ScrollView example (loads all list items at once even if they do not show on the screen) ...</Text>
+        <ScrollView>
+            {courseGoals.map((goal) => 
+              <View style={styles.goalItem} key={'key' + goal}>
+                <Text style={styles.goalText} >Goal x = {goal}</Text>
+              </View>)}
+        </ScrollView>
       </View>
     </View>
   );
 }
 
 // this is the style of app above since react native does not use CSS. Style objects, not inline style
+// this style does not cascade to other elements or inhirite from other elements.
 const styles = StyleSheet.create({
   appContainer: {
     flex: 1,
@@ -66,5 +95,15 @@ const styles = StyleSheet.create({
   },
   goalsContainer : {
     flex: 5,
+  },
+  goalItem: {
+    margin: 8,
+    padding:8,
+    borderRadius: 6,
+    borderWidth: 1,
+    backgroundColor: '#5e0acc',
+  },
+  goalText: {
+    color: 'white',
   }
 });
