0dc1d983bfe365e9b70d046563209edad3dd421f
Gaberoids <figueiropas@hotmail.com>
2023-04-20 20:32:59 -0600

    Improved style of input field and buttons on the modal.

--- components/GoalInput.js
+++ components/GoalInput.js
@@ -43,13 +43,14 @@ function GoalInput(props) {
                         <Button
                             title="Add Goal"
                             onPress={addGoalHandler}
+                            color="#5e0acc"
                         />
                     </View>
                     <View style={styles.button} >
-
                         <Button
                             title="Cancel"
                             onPress={props.onCancel}
+                            color="#f31282"
                         />
                     </View>
                 </View>
@@ -72,10 +73,13 @@ const styles = StyleSheet.create({
     },
     textInput: {
         borderWidth: 1,
-        borderColor: '#cccccc',
+        borderColor: '#e4d0ff',
+        backgroundColor: '#e4d0ff',
+        color: '#120438',
+        borderRadius: 6,
         width: '100%',
         margin: 8,
-        padding: 8,
+        padding: 16,
     },
     buttonContainer: {
         flexDirection: 'row',
